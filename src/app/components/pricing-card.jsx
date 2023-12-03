f8ec8a5acd1a6ef5b3f7ccf64b34c2fcac6d818e
David Bushi <bushidavid@gmail.com>
2023-12-03 12:10:30 +0100

    changes to pricing card

--- src/app/components/pricing-card.jsx
+++ src/app/components/pricing-card.jsx
@@ -2,36 +2,31 @@
 import Link from "next/link";
 import React from 'react';
 import { useRouter } from "next/navigation";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function PricingCard( { price, nickname, id } ) {    
 
   return (
-    <div className={`w-90 p-8 my-6 text-center h-[500px] rounded-3xl pr-16 shadow-xl ${nickname === "Pro" ? "bg-remotify-db scale-110" : "bg-white" }`}>
+    <div className={`w-60 p-6 my-4 mx-4 text-center h-[300px] border-1 border-slate-200 rounded-3xl pr-16 shadow-xl`}>
         <h1 className="text-black font-semibold text-2xl">{nickname}</h1>
         <p className="pt-2 tracking-wide">
-            <span className={`text-3xl font-semibold ${nickname === "Pro" ? "text-white" : ""}`}>{(price / 100)}</span>
+            <span className={`text-3xl font-semibold `}>{(price / 100)}</span>
             <span className="text-gray-400 align-top">EUR</span>
         </p>
         <hr className="mt-4 border-1"></hr>
         <div className="pt-8 h-full">
             <div>
-                <p className="font-semibold text-gray-400 text-left">
-                    <span className="material-icons align-middle">
-                        done
-                    </span>
+                <p className="font-semibold text-left">
+                    <FontAwesomeIcon icon={faCheck} style={{color: "#142c42",}} />
                     <span className="pl-2">
-                        Create <span className="text-black">one Job Post</span>
+                        Simply One Job Post
                     </span>
                 </p>
             </div>
             <div>
                 <p className="font-semibold text-gray-400 text-left pt-5">
-                    <span className="material-icons align-middle">
-                        done
-                    </span>
-                    <span className="pl-2">
-                        Flexible <span className="text-black">team meetings</span>
-                    </span>
+                   
                 </p>
             </div>
             <div>
