0129431deb7db32d08c19e1cd29b7dd4033cf78d
David Bushi <bushidavid@gmail.com>
2023-12-03 12:10:03 +0100

    changes to pricing cards and pricing page

--- src/app/api/get-products/route.js
+++ src/app/api/get-products/route.js
@@ -1,6 +1,8 @@
 import Stripe from "stripe";
 import { NextResponse } from "next/server";
 
+const revalidate = 0; 
+
 export async function GET() {
 
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
