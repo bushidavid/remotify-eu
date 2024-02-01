b40ada33a02e295f844250085ebed7b1531c278b
David Bushi <bushidavid@gmail.com>
2024-02-01 22:00:46 +0100

    react-icons, fixed footer

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,8 +1,11 @@
-import Link from "next/link"
-import {faInstagram, faLinkedinIn, faXTwitter} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Link from "next/link";
+import {faInstagram, faLinkedinIn, faXTwitter} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FaInstagram, FaLinkedinIn } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 export default function Footer() {
+
   return (
     <div className="flex flex-col w-full bg-remotify-db text-white items-center">
       <div className=" w-8/12 h-48 grid grid-cols-8 mt-20 relative bottom-0 justify-around">
@@ -23,10 +26,10 @@ export default function Footer() {
               <Link href="/">Create a company profile</Link>
               <Link href="/">Post a remote job</Link> 
           </div>
-          <div className="col-start-7 flex flex-row col-span-2 h-10 w-40 bg-white ">
-            <FontAwesomeIcon icon={faInstagram} style={{color: "#74C0FC",}} />
-            <FontAwesomeIcon icon={faLinkedinIn} style={{color: "#fffff7",}} />
-            <FontAwesomeIcon icon={faXTwitter} style={{color: "#fffff8",}} />
+          <div className="col-start-7 flex flex-row col-span-2 h-10 w-40 ">
+            <FaInstagram size={28} />
+            <FaLinkedinIn size={28}/>
+            <FaXTwitter size={28} />
           </div>
       </div>
       <div className="">&copy; 2024 RemotifyEurope - All Rights Reserved</div>
--- src/app/components/job-hero.jsx
+++ src/app/components/job-hero.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot, faCalendarDays, faBriefcase, faPersonCane } from '@fortawesome/free-solid-svg-icons';
 
