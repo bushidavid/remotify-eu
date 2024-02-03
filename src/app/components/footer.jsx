256585fa9008e4dc20614a5c7b46038ec08c2254
David Bushi <bushidavid@gmail.com>
2024-02-03 13:41:08 +0100

    updated footer

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -10,26 +10,26 @@ export default function Footer() {
     <div className="flex flex-col w-full bg-remotify-db text-white items-center">
       <div className=" w-8/12 h-48 grid grid-cols-8 mt-20 relative bottom-0 justify-around">
           <div className="flex flex-col col-start-1 col-span-2 grid-span ">
-              <p className="font-bold ">Company</p>
-              <Link href="/">About RemotifyEurope</Link>
-              <Link href="/">Careers</Link>
-              <Link href="/">Support</Link>
-              <Link href="/">Contact Us</Link>
+              <p className="font-bold text-slate-300">Company</p>
+              <Link href="/" className="text-slate-400 text-sm">About RemotifyEurope</Link>
+              <Link href="/" className="text-slate-400 text-sm">Careers</Link>
+              <Link href="/" className="text-slate-400 text-sm">Support</Link>
+              <Link href="/" className="text-slate-400 text-sm">Contact Us</Link>
           </div>
           <div className="flex flex-col col-start-3 col-span-2">
-            <p className="font-bold ">For candidates</p>
-              <Link href="/">Sign Up with RemotifyEurope</Link>
-              <Link href="/">Browse remote jobs</Link> 
+            <p className="font-bold text-slate-300">For candidates</p>
+              <Link href="/" className="text-slate-400 text-sm">Sign Up with RemotifyEurope</Link>
+              <Link href="/" className="text-slate-400 text-sm">Browse remote jobs</Link> 
           </div>
           <div className="flex flex-col col-start-5 col-span-2">
-            <p className="font-bold ">For companies</p>
-              <Link href="/">Create a company profile</Link>
-              <Link href="/">Post a remote job</Link> 
+            <p className="font-bold text-slate-300">For companies</p>
+              <Link href="/" className="text-slate-400 text-sm">Create a company profile</Link>
+              <Link href="/pricing" className="text-slate-400 text-sm">Post a remote job</Link> 
           </div>
-          <div className="col-start-7 flex flex-row col-span-2 h-10 w-40 ">
-            <FaInstagram size={28} />
-            <FaLinkedinIn size={28}/>
-            <FaXTwitter size={28} />
+          <div className="col-start-7 flex flex-row col-span-2 h-10 w-40">
+            <Link href="/" ><FaInstagram size={28}></FaInstagram></Link>
+            <Link href="https://www.linkedin.com/company/92876524/admin/feed/posts/?feedType=following" ><FaLinkedinIn size={28}/></Link>
+            <Link href="https://twitter.com/Remotify_Europe" ><FaXTwitter size={28} /></Link>          
           </div>
       </div>
       <div className="">&copy; 2024 RemotifyEurope - All Rights Reserved</div>
