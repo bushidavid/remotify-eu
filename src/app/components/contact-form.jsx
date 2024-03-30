caace93bc68a391ab633de54f0da031ed49c1515
David Bushi <bushidavid@gmail.com>
2024-03-30 09:08:16 +0100

    bot control contact form, layout changes

--- src/app/components/contact-form.jsx
+++ src/app/components/contact-form.jsx
@@ -1,11 +1,12 @@
 'use client';
 
-import { useState } from "react";
+import { use, useState } from "react";
 import {Textarea} from "@nextui-org/react";
 import {Input} from "@nextui-org/input";
 import {Select, SelectItem} from "@nextui-org/select";
 import { sendEmail } from "../actions/actions";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function ContactForm() {
 
@@ -19,6 +20,7 @@ export default function ContactForm() {
     const [success, setSuccess] = useState(false);
     const [error, setError] = useState(false);
     const [showButton, setShowButton] = useState(true);
+    const [botControl, setBotControl] = useState(true);
 
     const onSubmitForm = (e) => {
 
@@ -39,12 +41,11 @@ export default function ContactForm() {
     
 
     return (
-        <form className="flex flex-col justify-center max-w-4xl w-full h-full my-10" onSubmit={e => onSubmitForm(e)}>
+        <form className="flex flex-col justify-center max-w-4xl w-11/12 md:w-full h-full my-10" onSubmit={e => onSubmitForm(e)}>
             <div className="my-10 min-w-4/12">
                 <h1 className="text-4xl mb-4">Contact Us</h1>
-                <div className="flex flex-row items-center">
-                    <h2 className="text-md text-slate-600">Fill in the form below or contact us at: </h2>
-                    <Image alt="sales_email" src={'/sales_email.png'} width={210} height={100}/>
+                <div className="">
+                    <h2 className="text-md text-slate-600">Fill in the form below or contact us at: <Link href="mailto:sales@remotifyeurope.com">sales@remotifyeurope.com</Link> </h2>
                 </div>
                 <Input className="w-full" type="text" variant="underlined" label="Your Name" name="customerName" isRequired onChange={e => handleChange(e)}/>  
             </div>
@@ -53,6 +54,9 @@ export default function ContactForm() {
             </div>
             <div className="mb-10 min-w-4/12">
                 <Input className="w-full" type="text" variant="underlined" label="Subject" name="customerSubject" isRequired onChange={e => handleChange(e)}/>  
+            </div>
+            <div className="hidden mb-10 min-w-4/12">
+                <Input className="w-full" type="text" variant="underlined" label="control" name="control" isRequired onChange={() => setBotControl(false)}/>  
             </div>
             
             <Textarea
@@ -66,7 +70,7 @@ export default function ContactForm() {
                 name="customerMessage"
                 onChange={e => handleChange(e)}
             />
-            { showButton && <button type="submit" className="w-96 px-3 py-2 my-12 border-2 text-white rounded-lg bg-remotify-db">Submit</button> }
+            { showButton && <button type="submit" disabled={!botControl} className="w-96 disabled:opacity-50 px-3 py-2 my-12 border-2 text-white rounded-lg bg-remotify-db">Submit</button> }
             { success && <p className="text-green-900 my-2">Email sent successfully</p> }
             { error &&  <p className="text-red-900 my-2">Error sending email</p>}
         </form>
--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -6,10 +6,10 @@ export default function Footer() {
 
   return (
     <div className="flex flex-col w-full text-white items-center text-xs bg-remotify-db">
-      <div className="w-11/12 md:w-8/12 h-fit md:h-48 md:grid md:grid-cols-8 mt-5 md:mt-20 relative bottom-0 justify-around items-stretch text-xs">
+      <div className="w-11/12 md:w-11/12 h-fit md:h-48 md:grid md:grid-cols-8 mt-5 md:mt-20 relative bottom-0 justify-around items-stretch text-xs">
           <div className="flex flex-col md:col-start-1 md:col-span-2 md:grid-span md:items-center">
             <div className="flex flex-col font-poppins">
-              <p className="text-white font-medium text-[25px]  mb-4">Company</p>
+              <p className="text-white font-medium  text-sm md:text-lg mb-1 md:mb-2">Company</p>
               <Link href="/about" className="text-slate-400 text-xs hover:underline">About RemotifyEurope</Link>
               <Link href="/" className="text-slate-400 text-xs hover:underline">Careers</Link>
               <Link href="/contact-us" className="text-slate-400 text-xs hover:underline">Support</Link>
@@ -19,14 +19,14 @@ export default function Footer() {
           </div>
           <div className="flex flex-col md:col-start-3 md:col-span-2 md:items-center mt-3 md:mt-0">
             <div className="flex flex-col font-poppins">
-              <p className="text-white font-medium text-[25px] mb-4">For candidates</p>
+              <p className="text-white font-medium text-sm md:text-lg mb-1 md:mb-2">For candidates</p>
                 <Link href="/" className="text-slate-400 text-xs hover:underline">Sign Up with RemotifyEurope</Link>
                 <Link href="/" className="text-slate-400 text-xs hover:underline">Browse remote jobs</Link> 
             </div>
           </div>
           <div className="flex flex-col md:col-start-5 md:col-span-2 md:items-center mt-3 md:mt-0">
             <div className="flex flex-col font-poppins">
-              <p className="text-white font-medium text-[25px] mb-4">For companies</p>
+              <p className="text-white font-medium  text-sm md:text-lg mb-1 md:mb-2">For companies</p>
                 <Link href="/" className="text-slate-400 text-xs hover:underline">Create a company profile</Link>
                 <Link href="/contact-us" className="text-slate-400 text-xs hover:underline">Post a remote job</Link> 
             </div>
