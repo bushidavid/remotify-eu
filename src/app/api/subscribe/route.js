1f6afb9024183dd4db7f9e4cb032b83866020740
David Bushi <bushidavid@gmail.com>
2024-04-04 12:17:18 +0200

    add job_id to response of subscribe api

--- src/app/api/subscribe/route.js
+++ src/app/api/subscribe/route.js
@@ -30,7 +30,7 @@ export async function PUT( req ) {
         const response = await fetch(url, options);
         const json = await response.json();
 
-        return NextResponse.json({message: "Your email has been succesfully added to the mailing list. Welcome 👋" , status: 200, ok: true})
+        return NextResponse.json({message: "Your email has been succesfully added to the mailing list. Welcome 👋" , status: 200, ok: true, jobId: json.job_id})
     } catch (error) {
         console.log(error);
         return NextResponse.json({message: "Oups, there was a problem with your subscription, please try again or contact us", status: 500, ok: false})
