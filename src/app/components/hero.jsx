65bcb3a8018fec6013dd3c2c4784c03aa294622b
David Bushi <bushidavid@gmail.com>
2024-04-12 19:24:08 +0200

    last changes before go-live

--- src/app/components/hero.jsx
+++ src/app/components/hero.jsx
@@ -6,8 +6,8 @@ export default function Hero() {
   return ( 
     <div className="flex flex-col text-center justify-center gap-4 items-center mb-100 w-full relative h-[400px] md:h-[633px] bg-remotify-db">
         
-          <h1 className="text-white text-5xl w-full md:text-8xl md:w-8/12 font-medium">Find your Dream Job with Us!</h1>
-          <p className="text:md md:text-lg pt-1 md:w-6/12 w-8/12 text-gray-300">Navigate the World of Opportunities with Our Comprehensive Job Portal, Connecting Talented Individuals with their Dream Careers.</p>
+          <h1 className="text-white text-5xl w-full md:text-8xl md:w-8/12 font-medium">Find your Dream <span className="text-teal-400">Remote</span> Job with Us!</h1>
+          <p className="text:md md:text-lg pt-1 md:w-6/12 w-8/12 text-gray-300">Navigate the World of Opportunities with Our Comprehensive Job Portal, Connecting Talented European Individuals with their Dream Remote Careers.</p>
           <p className="text:md md:text-lg pt-1 md:w-6/12 w-8/12 text-gray-300">Start Searching Now!</p>
           <div className="block md:hidden">
                 <Link
