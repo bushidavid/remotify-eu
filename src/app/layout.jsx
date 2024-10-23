2fc8c8e54750cc755a5f27be2a399a9d3e550d11
David Bushi <bushidavid@gmail.com>
2024-10-23 16:32:04 +0200

    fix logo src for openGraph in homepage

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/navbar';
 import SessProvider from './components/session-provider';
 import SubscribeModal from './components/subscribe-modal';
 import { GoogleAnalytics } from '@next/third-parties/google';
+import logo from '../../public/Logo.jpg'
 
 
 
@@ -15,7 +16,7 @@ export const metadata = {
     title: 'RemotifyEurope - Home',
     description: 'Find the best remote jobs in Europe',
     images: [
-      '/Logo.jpg' // Path to the Open Graph image in your public directory
+      logo // Path to the Open Graph image in your public directory
     ],
   },
 }
