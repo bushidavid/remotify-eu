51138154d6a3411b3a7b38ec2153a382fe92cf50
David Bushi <bushidavid@gmail.com>
2024-11-23 16:19:26 +0100

    rework pricing page

--- src/app/components/pricing-card.jsx
+++ src/app/components/pricing-card.jsx
@@ -10,40 +10,75 @@ import { BsCurrencyEuro } from "react-icons/bs";
 export default function PricingCard( { price, nickname, id, features, description} ) {   
 
   return (
-    <div className={`w-80 flex flex-col py-4 px-10 shadow-xl rounded-xl border-1 border-slate-100    ${nickname === 'Enterprise' ? "bg-teal-50 border-slate-200" : ""}`}>
+    <div className={`w-80 flex flex-col py-4 px-4 shadow-xl rounded-xl border-1 border-slate-300    ${nickname === 'Enterprise' ? "border-1 bg-teal-50 border-teal-500" : ""}`}>
         <div className="w-full ">
-            <div className={`w-fit rounded-lg bg-remotify-lb text-white py-1 px-2 mb-1 text-sm ${nickname == 'Enterprise' ? "" : "invisible"}`}><p>Best Deal</p></div>
+            <div className={`flex items-end justify-end flex-grow mb-1 text-base font-light text-right ${nickname == 'Enterprise' ? "" : "invisible"}`}><p className="bg-remotify-lb w-fit text-white py-1 px-2 rounded-lg">Best Deal</p></div>
             <h1 className="text-black font-semibold text-2xl">{nickname}</h1>
             <h2 className="text-slate-600 text-sm">{description}</h2>
-            {nickname == "Custom" ? ( <p className={` mb-4 mt-10 text-3xl`}>Contact Us</p>) : ( <p className={` mb-4 mt-10 text-3xl`}>€ {(price / 100)}</p>)}
+            {nickname === "Custom" ? (
+                <p className="mb-2 mt-10 text-3xl">Contact us for:</p>
+                ) : (
+                <p className="mb-2 mt-10 text-3xl text-slate-700">
+                    € {Math.floor(price / 100)}
+                    <span
+                        className="text-sm"
+                        style={{ verticalAlign: 'top', fontSize: '0.7em', position: 'relative', top: '0.2em' }}
+                        >
+                        {(price % 100).toString().padStart(2, '0')}
+                    </span>
+                </p>
+            )}
+
 
         </div>
-        <div className="w-full">
+        
+        { nickname != "Custom" ? 
+            (<div className="h-full flex flex-col items-start w-full">
+                {/* {nickname != "Basic" && <h1 className="text-sm font-medium">This includes:</h1>} */}
+                {features?.map(feature => {
+                    return (
+                    <div key={Math.random()} className="flex flex-row items-center mt-2 mb-2">
+                        <CiCircleCheck />
+                        <p className="text-sm ml-1" key={Math.random()}>{feature.name}</p>
+                    </div>)
+                })}   
+            </div>)
+            : (
+                <div className="h-full flex flex-col items-start w-full">
+                    <div className="flex flex-row mt-2 mb-2 items-center">
+                        <CiCircleCheck />
+                        <p className="text-sm ml-1" >Bundles</p>
+                        
+                    </div>
+                    <div className="flex flex-row mt-2 mb-2 items-center">
+                        <CiCircleCheck />
+                        <p className="text-sm ml-1" >Discounts</p>
+                        
+                    </div>
+                    <div className="flex flex-row mt-2 mb-2 items-center">
+                        <CiCircleCheck />
+                        <p className="text-sm ml-1" >Quotations</p>
+                        
+                    </div>
+                </div>
+            )
+        }   
+
+        <div className="w-full mt-6 text-center">
             {nickname == "Custom" ?
                 (
-                    <Link href={`/contact-us`} className="py-4 px-4 my-6 bg-remotify-db rounded-lg text-white">
-                            Ask For a Quote
+                    <Link href={`/contact-us`} className="block py-4 px-4 my-2 bg-remotify-db text-lg rounded-lg font-light text-white w-full">
+                            Contact Us For
                     </Link>
+                    
                 ) 
                 : (
-                    <Link href={`/newjob/${id}`} className="py-4 px-4 my-6 bg-remotify-db rounded-lg text-white">
+                    <Link href={`/newjob/${id}`} className="block py-4 px-4 my-2 bg-remotify-db text-lg font-light rounded-lg text-white w-full">
                         Choose Plan
                     </Link>
                 )
             }
         </div>
-        { nickname != "Custom" && 
-            <div className="pt-8 h-full flex flex-col items-start w-full">
-                {/* {nickname != "Basic" && <h1 className="text-sm font-medium">This includes:</h1>} */}
-                {features?.map(feature => {
-                    return (
-                    <div key={Math.random()} className="flex flex-row mt-2 mb-2">
-                        <CiCircleCheck />
-                        <p className="text-sm ml-2" key={Math.random()}>{feature.name}</p>
-                    </div>)
-                })}   
-            </div>
-        }   
     </div>
   )
 }
--- src/app/pricing/page.jsx
+++ src/app/pricing/page.jsx
@@ -70,18 +70,18 @@ export default async function Page() {
                     
                     <main className="flex-grow">
                         <div className="font-sans mt-10">
-                            <div className="flex justify-center items-center">
+                            <div className="flex flex-col justify-center items-center">
                                 <div className="">
-                                    <div className="text-center font-semibold">
-                                        <h1 className="text-5xl">
-                                            <span className="text-remotify-lb tracking-wide">Flexible </span>
-                                            <span className='text-remotify-db'>Plans</span>
+                                    <div className="flex flex-col items-center text-center font-semibold">
+                                        <h1 className="text-4xl md:text-6xl text-remotify-db w-11/12 md:max-w-none">
+                                            Hire Top Remote <span className="text-remotify-lb tracking-wide" >Talent</span> Across Europe 
+                                            
                                         </h1>
-                                        <p className="pt-6 text-xl text-gray-600 font-normal w-full px-8 md:w-full">
-                                            Choose a plan that works best for you and your team.
+                                        <p className="pt-6 text-base md:text-xl text-slate-500 font-normal w-full px-8 md:w-full max-w-6xl">
+                                            At RemotifyEurope, we connect companies with a growing pool of talented candidates across Europe and European time zones. Our streamlined job board ensures your remote opportunities get seen by the right people — ambitious, skilled professionals ready to make a difference in your business.
                                         </p>
                                     </div>
-                                    <div className="my-10 flex md:flex-row flex-col gap-x-4">
+                                    <div className="my-10 flex md:flex-row items-center md:justify-center md:items-stretch  flex-col md:gap-x-4 gap-y-4 md:gap-y-0">
                                     {priceCards.map((card, index) => (
                                             <PricingCard
                                                 key={card.id}
@@ -100,6 +100,67 @@ export default async function Page() {
                                             features={productMap[prices[1]?.product] || []}
                                         />
                                     </div>
+                                </div>
+                                <div className='bg-remotify-db w-full h-fit'>
+                                    <div className="flex flex-col items-center text-center font-semibold py-10 gap-y-4">
+                                        <h2 className="text-3xl md:text-5xl text-slate-200">
+                                            <span className="text-remotify-lb tracking-wide" >Special Offer: </span>Get Started for Free!
+                                            
+                                        </h2>
+                                        <p className="pt-6 text-base md:text-xl text-slate-300 font-normal w-full px-8 md:w-full max-w-5xl">
+                                            To help you discover the benefits of hiring with RemotifyEurope, we’re offering 3 FREE job posts for new companies in their first month.
+                                        </p>
+                                        <p className="pt-6 text-base md:text-slate-300 font-normal w-full px-8 md:w-full max-w-6xl">
+                                            Post your first listing today and see why businesses trust us to find top European talent.
+                                        </p>
+                                        <Link
+                                            href={'/pricing'}
+                                            className="font-poppins text-xl my-4 bg-remotify-lb text-remotify-db font-normal rounded-md py-2 px-4"
+                                        >
+                                            Post a Job Now!
+                                        </Link>
+                                    </div>
+                                    
+                                </div>
+                                <div className='w-full flex flex-col items-center py-10'>
+                                    <div className="flex flex-col items-center text-center font-semibold mb-10 gap-y-4">
+                                        <h2 className="text-3xl md:text-5xl text-remotify-db">
+                                           How We Help You  <span className="text-remotify-lb tracking-wide" >Stand Out</span>
+                                        </h2>
+                                        <p className="pt-6 text-base md:text-xl text-slate-600 font-normal w-full px-8 md:w-full max-w-5xl">
+                                        At RemotifyEurope, we understand that every job is unique. That&apos;s why we go the extra mile to showcase your opportunities to the most relevant candidates.
+                                        </p>
+                                        
+                                    </div>
+                                    <div className='flex items-center justify-center md:max-w-7xl '>
+                                        <div className='w-8/12 md:w-6/12'>
+                                            <ul className='list-disc list-outside leading-8 md:leading-[40px] text-slate-600 text-sm md:text-xl'>
+                                                <li><span className='font-bold'>Personalized Support:</span> Need assistance crafting the perfect job listing? We&apos;re here to help.
+                                                </li>
+                                                <li><span className='font-bold'>Quality Over Quantity:</span> Unlike other platforms, we focus on delivering your listing to candidates who meet your needs.
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='bg-remotify-lb w-full'>
+                                    <div className="flex flex-col items-center text-center font-semibold py-10 gap-y-4">
+                                        <h2 className="text-3xl md:text-5xl text-remotify-db">
+                                            <span className="tracking-wide" >Why Start </span>Now?
+                                            
+                                        </h2>
+                                        <p className="pt-6 text-base md:text-xl text-slate-700 font-normal w-full px-8 md:w-full max-w-5xl">
+                                        The earlier you list your job, the sooner you&apos;ll find the ideal candidate. Don&apos;t let great talent slip away — post your job today and join our growing network of companies that hire smarter.
+                                        </p>
+                                        
+                                        <Link
+                                            href={'/pricing'}
+                                            className="font-poppins text-xl my-4 bg-remotify-db text-white font-normal rounded-md py-2 px-4"
+                                        >
+                                            Post a Job Now!
+                                        </Link>
+                                    </div>
+                                    
                                 </div>
                             </div>
                         </div>
